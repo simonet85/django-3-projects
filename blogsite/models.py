296addfbaf825e79a4a32298f55b79f99abd9e91
simonet85 <christian.85@live.fr>
2020-12-05 16:16:49 +0100

    create template,list and detail and get_absolute_url

--- blogsite/models.py
+++ blogsite/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+#Creating model managers
+class PublishedManager( models.Manager ) :
+    def get_queryset( self ) :
+        return super( PublishedManager, self ).get_queryset().filter( status='published' )
+
+class Post( models.Model ) :
+    STATUS_CHOICES = (
+        ( 'draft', 'Draft'),
+        ( 'published', 'Published'),
+    )
+
+    title = models.CharField( max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey( User, on_delete=models.CASCADE, related_name='blogsite_posts')
+    body = models.TextField()
+    publish = models.DateTimeField( default=timezone.now )
+    created = models.DateTimeField( auto_now_add=True )
+    updated = models.DateTimeField( auto_now=True )
+    status = models.CharField( max_length=10, choices=STATUS_CHOICES, default='draft')
+    
+    objects = models.Manager() #the default manager
+    published = PublishedManager() #Our custom manager
+    
+   
+    class Meta :
+        ordering = ( '-publish', )
+        
+    def __str__(self) :
+        return  self.title
+    
+    #reverse() method, which allows you to build URLs by their name and pass optional parameters.
+    def get_absolute_url( self ) :
+        return reverse(  'blogsite:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug] )
+    
+
+    
+    
+
+    
+    
