4e9e7d3d0997aa8823ed0de94ab15b23c9e0d287
simonet85 <christian.85@live.fr>
2020-12-05 20:03:55 +0100

    change the function based view to class based view

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
@@ -21,6 +22,12 @@ def post_list( request ) :
 def post_detail( request, year, month, day, post ) :
     post = get_object_or_404(Post, slug=post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
     return render( request, 'blogsite/post/detail.html', {'post' : post})
+
+class PostListView(ListView) :
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blogsite/post/list.html'
 
 
 
