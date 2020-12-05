45a4faa55afe39ef94bac91c83218fe5da6f505d
simonet85 <christian.85@live.fr>
2020-12-05 19:40:25 +0100

    pagination features added

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list( request ) :
-    posts = Post.published.all()
-    return render( request, 'blogsite/post/list.html',{'posts' : posts} )
+    #posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator( object_list, 3 ) # 3posts in each in each page
+    page = request.GET.get( 'page' ) #page Number
+    try:
+        posts = paginator.get_page( page ) #page Object
+    except PageNotAnInteger :
+        #If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage :
+        #If page is out of range deliver last page of results
+        posts = paginator.page( paginator.num_pages )
+   
+    return render( request, 'blogsite/post/list.html',{'page':page, 'posts' : posts} )
 
 def post_detail( request, year, month, day, post ) :
     post = get_object_or_404(Post, slug=post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
     return render( request, 'blogsite/post/detail.html', {'post' : post})
+
+
+
+        
+    
