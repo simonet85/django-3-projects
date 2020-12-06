5bdf10738187e9befd86020375d866755c42e193
simonet85 <christian.85@live.fr>
2020-12-06 16:31:14 +0100

    create forms from models

--- blogsite/forms.py
+++ blogsite/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm( forms.Form ) :
     name = forms.CharField( max_length = 25 )
@@ -6,3 +7,9 @@ class EmailPostForm( forms.Form ) :
     to = forms.EmailField()
     comments = forms.CharField( required = False, widget = forms.Textarea )
     
+#create forms from models (Django has two base classes to build forms: Form and ModelForm)
+class CommentForm( forms.ModelForm ) :
+    #To create a form from a model, you just need to indicate which model to use to build the form in the Meta class of the form.
+    class Meta :
+        model = Comment
+        fields = ('name', 'email', 'body')
