a38386377d1e34f79c7cab0b3a91fd558df44f25
simonet85 <christian.85@live.fr>
2020-12-06 16:53:23 +0100

    Handling ModelForms in views

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
  
 def post_list( request ) :
@@ -23,7 +23,28 @@ def post_list( request ) :
 
 def post_detail( request, year, month, day, post ) :
     post = get_object_or_404(Post, slug=post, status = 'published', publish__year = year, publish__month = month, publish__day = day)
-    return render( request, 'blogsite/post/detail.html', {'post' : post})
+    
+    #List of active comments for this post
+    comments = post.comments.filter( active=True )
+    new_comment = None
+    if request.method == 'POST' :
+        #A comment was posted
+        comment_form = CommentForm( data=request.POST )
+        if comment_form.is_valid() :
+            #Create Comment object but don't save to databse yet
+            new_comment = comment_form.save( commit=False )
+            #Assign the current post to the comment
+            new_comment.post = post
+            #Save the comment to the database
+            new_comment.save()
+    else :
+        comment_form = CommentForm()
+    return render( request, 'blogsite/post/detail.html', {
+        'post' : post, 
+        'comment': comments, 
+        'new_comment' : new_comment, 
+        'comment_form' : comment_form
+        })
 
 class PostListView(ListView) :
     queryset = Post.published.all()
@@ -58,6 +79,7 @@ def post_share( request, post_id ) :
         
     return render( request, 'blogsite/post/share.html', {'post' : post, 'form' : form, 'sent' : sent})
 
+#Handling ModelForms in views
 
 
         
