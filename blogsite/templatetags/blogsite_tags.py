7b2bf35bb43e2421a9057016f8d6a0de2ccdeb7f
simonet85 <christian.85@live.fr>
2020-12-07 10:55:24 +0100

    include template inclusion

--- blogsite/templatetags/blogsite_tags.py
+++ blogsite/templatetags/blogsite_tags.py
@@ -2,7 +2,13 @@ from django import template
 from ..models import Post
 
 register = template.Library()
-#create simple template tag
+#create simple template tag simple_tag
 @register.simple_tag
 def total_posts() :
     return Post.published.count()
+
+#create template inclusion inclusion_tag
+@register.inclusion_tag( 'blogsite/post/latest_posts.html' )
+def show_latest_posts( count=5 ) :
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts' : latest_posts}
