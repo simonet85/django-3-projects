ba8f3dfe3a771a4de94c5a2bb5daf34ecdfea5c5
simonet85 <christian.85@live.fr>
2020-12-07 13:00:24 +0100

    adding a sitemap to your site

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blogsite.sitemaps import PostSitemap
 
+sitemaps = {
+    'posts' : PostSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blogsite/', include('blogsite.urls'))
+    path('blogsite/', include('blogsite.urls', namespace='blogsite')),
+    path('sitemap.xml', sitemap, {'sitemaps' : sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
--- blogsite/templatetags/blogsite_tags.py
+++ blogsite/templatetags/blogsite_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 #create simple template tag simple_tag
@@ -19,3 +21,8 @@ def get_most_commented_post( count=5 ) :
     return Post.published.annotate(
         total_comments = Count('comments')
     ).order_by('-total_comments')[:count]
+    
+#Custom template filters
+@register.filter( name='markdown' )
+def markdown_format( text ) :
+    return mark_safe( markdown.markdown( text ))
